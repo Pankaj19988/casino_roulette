77668d4aab585f8b39f9941e57665ac4e269b13d
Pankaj19988 <pankajmalankiya@gmail.com>
2023-09-01 11:41:31 +0530

    code added

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./styles.css";
+import DraggeableForm from "./DraggeableForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
+
+function App() {
+
+ 
+  const [mustSpin, setMustSpin] = useState(false);
+  const [selectedNo, setSelectedNo] = useState([])
+  return (
+
+    <div>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <div className="App">
+        <DraggeableForm mustSpin={mustSpin} setMustSpin={setMustSpin} selectedNo={selectedNo} setSelectedNo={setSelectedNo} />
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
+
